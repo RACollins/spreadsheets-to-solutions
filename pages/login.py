4a1e5431080ed8f02e93ffe73796928514c9b6eb
RACollins <richardcollins128@gmail.com>
2025-09-13 10:59:32 +0900

    Refactor app structure and enhance login functionality
    - Introduced a main function in app.py to encapsulate the app logic.
    - Improved login mechanism in login.py with password validation and session management.
    - Updated logout functionality to clear password state.
    - Added .streamlit/secrets.toml to .gitignore for sensitive information management.

--- pages/login.py
+++ pages/login.py
@@ -1,18 +1,45 @@
 import streamlit as st
+import time
+
+
+def check_password():
+    """Checks whether the user has entered the correct password."""
+    if st.session_state.get("password"):
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+        else:
+            st.session_state["password_correct"] = False
+    else:
+        # No password entered
+        st.session_state["password_correct"] = False
+    return st.session_state["password_correct"]
 
 
 def login():
-    if st.button("Log in"):
-        st.session_state.logged_in = True
-        st.rerun()
+    if not st.session_state.get("password_correct", False):
+        st.text_input("Password", type="password", key="password")
+
+        # Check if user pressed Enter (password field has value) or clicked login button
+        login_button = st.button("Log in")
+        login_triggered = login_button or st.session_state.get("password")
+
+        if login_triggered:
+            if check_password():
+                st.session_state["password_correct"] = True
+                st.session_state["logged_in"] = True
+                del st.session_state["password"]  # don't keep password in session
+                st.success("Logged in successfully")
+                time.sleep(1)
+                st.rerun()
+            else:
+                st.error("Password incorrect")
+                st.stop()
 
 
 def main():
     left_margin, main_content, right_margin = st.columns([1, 5, 1])
     with main_content:
-        logo = st.image(
-            "data/assets/marketing-magic-high-resolution-logo-transparent.png"
-        )
+        st.image("data/assets/marketing-magic-high-resolution-logo-transparent.png")
         login()
 
 
--- pages/logout.py
+++ pages/logout.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 def logout():
     if st.button("Log out"):
-        st.session_state.logged_in = False
+        st.session_state["logged_in"] = False
+        del st.session_state["password_correct"]
         st.rerun()
 
 def main():
